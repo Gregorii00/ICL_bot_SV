3fd5bc16bd4eec027c8158a0e1c6f460f8d448ae
Gregorii <107300093+Gregorii00@users.noreply.github.com>
2024-11-19 01:47:23 +0300

    Update additional.py

--- additional.py
+++ additional.py
@@ -13,7 +13,9 @@ report_week_name2 = 'Некорректные обращения. Рейтинг
 # Для таблиц
 
 scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials_file = 'icl-bot-sv-441816-3f5b548d87bf.json'
+credentials_file = 'icl-bot-sv-441816-89d3766b0056.json'
 spreadsheet_id_wiretapping = '1aV4TfDy9b96KkaLvFRiqIHjZLXlqXoqZfQg5Jdyf9hI'
-spreadsheet_id_schedule = '1MQ0hNCotH-ou1jp3ODHeBhdww1w6UhG8X588-n-db6c'
-url = 'https://docs.google.com/spreadsheets/d/1aV4TfDy9b96KkaLvFRiqIHjZLXlqXoqZfQg5Jdyf9hI/'
+# spreadsheet_id_schedule = '1MQ0hNCotH-ou1jp3ODHeBhdww1w6UhG8X588-n-db6c'
+spreadsheet_id_schedule = '1KT02P__u-wbz0g1O2bk9umytUbz-WNm5Iaec5sJqb-s'
+# url = 'https://docs.google.com/spreadsheets/d/1MQ0hNCotH-ou1jp3ODHeBhdww1w6UhG8X588-n-db6c/'
+url = 'https://docs.google.com/spreadsheets/d/1KT02P__u-wbz0g1O2bk9umytUbz-WNm5Iaec5sJqb-s/'
